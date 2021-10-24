546c628821c0f65741c8fa8293ea5ae7a8abb72a
Abel Alejandro Fleitas <abel.fleitas@ntsprint.com>
2021-10-24 18:08:33 -0400

    Upload project

---
+++ APICore/APICore.Test/Unit/SettingServiceTests/Providers/SqliteSettingServiceTests.cs
@@ -0,0 +1,14 @@
+﻿using APICore.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace APICore.Test.SettingServiceTests.Providers
+{
+    public class SqliteSettingServiceTests : SettingServiceTests
+    {
+        public SqliteSettingServiceTests() : base(new DbContextOptionsBuilder<CoreDbContext>()
+                                                 .UseSqlite("Filename=Test.db")
+                                                 .Options)
+        {
+        }
+    }
+}
